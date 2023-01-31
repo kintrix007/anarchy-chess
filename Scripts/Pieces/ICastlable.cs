6cb5b580b2af0b1f9030cbf44ea75341b030ace5
kintrix007 <boldizsar02@gmail.com>
2023-01-31 01:55:07 +0100

    Added promotable and castlable interfaces
    This will likely change in the future

---
+++ Scripts/Pieces/ICastlable.cs
@@ -0,0 +1,4 @@
+namespace AnarchyChess.Scripts.Pieces
+{
+    public interface ICastlable {}
+}
---
+++ Scripts/Pieces/IPromotable.cs
@@ -0,0 +1,9 @@
+namespace AnarchyChess.Scripts.Pieces
+{
+    public interface IPromotable
+    {
+        //? Not really sure how this should look like
+        //? Maybe an IPiece[] field?
+        //object[] PossiblePromotions { get; }
+    }
+}
