f8c1d30b52445ee3b071d8f230dd4009d26df3dd
kintrix007 <boldizsar02@gmail.com>
2023-02-02 22:13:36 +0100

    Added demo to castling and en passant

--- Scripts/BoardObj.cs
+++ Scripts/BoardObj.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Linq;
 using AnarchyChess.Scripts.Boards;
 using AnarchyChess.Scripts.Games;
 using AnarchyChess.Scripts.Moves;
+using AnarchyChess.Scripts.PieceHelper;
 using AnarchyChess.Scripts.Pieces;
 using Godot;
 
@@ -10,6 +12,71 @@ namespace AnarchyChess.Scripts
     public class BoardObj : Node
     {
         public override void _Ready()
+        {
+            var game = new Game(@"
+r n b q k b n r
+p p p p - p p p
+- - - - - - - -
+- - - - P - - -
+- - - - - - - -
+- - - - - - - -
+P P P P - P P P
+R - - - K - - R
+".Trim().ParseBoard());
+
+            var kingPos = new Pos("E1");
+            var king = game.Board[kingPos];
+            var moves = king.GetMoves(game, kingPos).Where(game.ValidateMove).ToList();
+            GD.Print(game.Board.DumpTemplate());
+            GD.Print("------");
+            game.ApplyMove(moves[3]);
+            GD.Print(game.Board.DumpTemplate());
+            GD.Print("------");
+            game.ApplyMove(Move.Relative(new Pos("D7"), new Pos(0, -2)));
+            GD.Print(game.Board.DumpTemplate());
+            GD.Print("------");
+
+            var pawn = game.Board[new Pos("E5")];
+            moves = pawn.GetMoves(game, new Pos("E5")).Where(game.ValidateMove).ToList();
+            GD.Print(string.Join(", ", moves));
+
+            game.ApplyMove(moves.Last());
+            GD.Print(game.Board.DumpTemplate());
+            GD.Print("------");
+        }
+
+        private static void SimulateRandomGame()
+        {
+            var game = new Game(BoardTemplates.Standard());
+
+            foreach (var i in Enumerable.Range(0, 100))
+            {
+                var move = GetRandomMove(game, i % 2 == 0 ? Side.White : Side.Black);
+                game.ApplyMove(move);
+                
+                GD.Print(game.Board.DumpTemplate());
+                GD.Print($"{game.Scores[Side.White]} ------ {game.Scores[Side.Black]}");
+            }
+        }
+        
+        private static Move GetRandomMove(Game game, Side side)
+        {
+            var rnd = new Random();
+
+            while (true)
+            {
+                var pos = new Pos(rnd.Next(8), rnd.Next(8));
+                var piece = game.Board[pos];
+                if (piece == null) continue;
+                if (piece.Side != side) continue;
+                
+                var moves = piece.GetMoves(game, pos).Where(game.ValidateMove).ToList();
+                if (moves.Count == 0) continue;
+                return moves[rnd.Next(moves.Count)];
+            }
+        }
+        
+        private static void OtherThingIGuess()
         {
             var board = BoardTemplates.Standard();
             var game = new Game(board);
@@ -44,8 +111,10 @@ namespace AnarchyChess.Scripts
             }
             
             GD.Print(game.Board.DumpTemplate());
-            
-            return;
+        }
+        
+        private static void ReallyDontKnow()
+        {
             var board1 = BoardTemplates.Standard();
             var template = board1.DumpTemplate();
 
